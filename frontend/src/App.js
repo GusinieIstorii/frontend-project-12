249dcee158ce1dfeda2f5dc7392635d45914940f
Kseniia <ksenia.guseva789@gmail.com>
2023-09-26 11:26:04 +0300

    middle of the step 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
 import PageOne from './Components/Pages';
 import { Page404 } from './Pages/Page404';
 import { LoginPage } from './Pages/LoginPage';
+import AuthContext from './Contexts/AuthContext';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 function App() {
   return (
+    <AuthProvider>
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<Page404 />} />
         <Route path="one" element={<PageOne />} />
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<PageOne />} />
         <Route path="login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,9 +1,13 @@
 import { Formik, Form, Field } from 'formik';
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
+import AuthContext from '../Contexts/AuthContext';
+import routes from '../hooks/routes';
 
 const LoginSchema = Yup.object().shape({
-  nickname: Yup.string()
+  username: Yup.string()
     .min(2, 'Минимум 2 буквы')
     .max(50, 'Максимум 50 букв')
     .required(),
@@ -13,27 +17,51 @@ const LoginSchema = Yup.object().shape({
     .required(),
 });
 
-const BuildForm = () => (
+const LoginForm = () => {
+  const { loggedIn, logIn, logOut } = useContext(AuthContext);
+  const [authFailed, setAuthFailed] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const submitForm = async (values, { setSubmitting }) => {
+    setAuthFailed(false);
+    console.log('hi');
+    console.log(values);
+    try {
+      const res = await axios.post(routes.loginPath(), values);
+      console.log(res.data); // => { token: ..., username: 'admin' }
+      // localStorage.setItem('userId', JSON.stringify(res.data));
+      logIn();
+      // navigate('/');
+    } catch (err) {
+      setSubmitting(false);
+      if (err.isAxiosError && err.response.status === 401) {
+        setAuthFailed(true);
+        // inputRef.current.select();
+        return;
+      }
+      throw err;
+    }
+  };
+
+  return(
   <>
   <h1>Войти</h1>
     <Formik
-      initialValues={{ nickname: "", password: "" }}
+      initialValues={{ username: "", password: "" }}
       validationSchema={LoginSchema}
-      onSubmit={({ setSubmitting }) => {
-      console.log("Form is validated! Submitting the form...");
-      setSubmitting(false);
-      }}>
+      onSubmit={submitForm}>
         {({ errors, touched }) => (
     <Form>
       <div className="form-group">
-        <label htmlFor="nickname">Ваш ник</label>
+        <label htmlFor="username">Ваш ник</label>
         <Field
           type="text"
-          name="nickname"
+          name="username"
           className="form-control"
         />
-        {errors.nickname && touched.nickname ? (
-            <div>{errors.nickname}</div>
+        {errors.username && touched.username ? (
+            <div>{errors.username}</div>
           ) : null}
       </div>
       <div className="form-group">
@@ -44,7 +72,7 @@ const BuildForm = () => (
           className="form-control"
         />
         {errors.password && touched.password ? (
-            <div>{errors.nickname}</div>
+            <div>{errors.username}</div>
           ) : null}
       </div>
       <button type="submit">Войти</button>
@@ -52,8 +80,8 @@ const BuildForm = () => (
   )}
     </Formik>
   </>
-);
+)};
 
-const LoginForm = () => BuildForm();
+// const LoginForm = () => BuildForm();
 
 export default LoginForm;
